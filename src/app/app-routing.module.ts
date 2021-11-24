86725d8b4347a5262735e20ca0a70543e52b1c38
Jhon Aparicio <jj-aparicior@hotmail.com>
2021-11-23 20:53:44 -0500

    proyecto base con angular y materialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'index',
+  component: IndexComponent,
+},
+{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: '/index'
+},
+{
+  path: 'error',
+  component: ErrorComponent,
+},{
+  path: '**',
+  redirectTo: '/error'
+},
+{
+  path: 'seguridad',
+  loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+},{
+  path: 'admin',
+  loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+},{
+  path: 'encomiendas',
+  loadChildren: () => import('./modulos/encomiendas/encomiendas.module').then(m => m.EncomiendasModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
