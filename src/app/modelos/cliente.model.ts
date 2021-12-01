e3fe70acfaaa7c897cbe6c1a53ea5ca3ed48d5f7
Jhon Aparicio <jj-aparicior@hotmail.com>
2021-11-30 20:58:10 -0500

    Creación de modelos y servicios de cada entidad

---
+++ src/app/modelos/cliente.model.ts
@@ -0,0 +1,12 @@
+export class ClienteModelo{
+    id?: String;
+    cedula?: String;
+    nombre?: String;
+    apellidos?: String;
+    pais?: String
+    ciudad?: String
+    departamento?: String
+    direccion?: String
+    telefono?: string;
+    email?: string;
+  }
---
+++ src/app/modelos/encomienda.model.ts
@@ -0,0 +1,7 @@
+export class EncomiendaModelo{
+    id?: String;
+    descripcion?: String;
+    peso?: number;
+    tipo?: String;
+    presentacion?: String;
+  }
